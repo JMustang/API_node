b59e8cdabe25c8faf6a16ea691c52bb031933196
Junior <eecfredes@gmail.com>
2021-08-10 20:40:14 -0300

    feat: express add, servidor criado

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 1. yarn init -y
 2. yarn add typescript ts-node ts-node-dev @types/node
+3. yarn add express @types/express
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("teste");
+import express, { Request, Response } from "express";
+
+const PORT = process.env.PORT || 8891;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    msg: "OK!!",
+  });
+});
+
+app.listen(PORT, () => console.log("Servidor iniciado na porta " + PORT));
