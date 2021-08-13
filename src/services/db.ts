1144311bbd24a830e679f5c842b92c36c85c76c1
Junior <eecfredes@gmail.com>
2021-08-12 23:34:20 -0300

    feat: encapisulando querys

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,4 +1,21 @@
+import sqlite3 from "sqlite3";
+
 const DATABASE_FILE = process.env.DATABASE_FILE;
 if (!DATABASE_FILE) throw new Error("No DATABASE_FILE environment variable");
 
-export const openConnection = () => {};
+export const openConnection = () => {
+  let db = new sqlite3.Database(DATABASE_FILE);
+  return db;
+};
+
+export const dbQuerry = (query: string, params?: any[]) => {
+  let db = openConnection();
+  return new Promise<any[]>((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  }).finally(() => {
+    db.close();
+  });
+};
